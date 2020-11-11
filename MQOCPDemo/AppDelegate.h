883ebf93c9b963db0ed443cb25aacf256c49daea
MQTwsit <mq943691300@163.com>
2020-11-11 16:49:15 +0800

    Initial Commit

---
+++ MQOCPDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MQOCPDemo
+//
+//  Created by ma qi on 2020/11/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MQOCPDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MQOCPDemo
+//
+//  Created by ma qi on 2020/11/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
